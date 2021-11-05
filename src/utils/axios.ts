d5e91c72bbb5dc8973317a32f8cd835a8491fadb
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-05 12:56:03 +0600

    base url changed

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 
 // export const axios = Axios.create({baseURL: 'http://3.22.167.8'});
-const axios = Axios.create({ baseURL: 'https://blooming-beach-78806.herokuapp.com' });
+// const axios = Axios.create({ baseURL: 'https://blooming-beach-78806.herokuapp.com' });
+const axios = Axios.create({ baseURL: 'http://3.144.214.224/' });
 
 export default axios;
