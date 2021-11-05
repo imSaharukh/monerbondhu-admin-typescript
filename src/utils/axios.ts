b4c7555165db0bc4fc9042ef63dbcb8a14dc8f1d
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-05 18:08:52 +0600

    ssl enabled base url added

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,6 +2,6 @@ import Axios from 'axios';
 
 // export const axios = Axios.create({baseURL: 'http://3.22.167.8'});
 // const axios = Axios.create({ baseURL: 'https://blooming-beach-78806.herokuapp.com' });
-const axios = Axios.create({ baseURL: 'http://3.144.214.224/' });
+const axios = Axios.create({ baseURL: 'https://3.144.214.224.nip.io/' });
 
 export default axios;
