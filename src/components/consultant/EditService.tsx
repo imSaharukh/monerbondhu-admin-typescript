9c4e1a14c6adb04b16d5c7416ef12ab89a714ff1
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-16 22:10:14 +0600

    edit service of consultant

--- src/components/consultant/EditService.tsx
+++ src/components/consultant/EditService.tsx
@@ -1,3 +1,4 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -12,8 +13,12 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TextField from '@material-ui/core/TextField';
+import CheckIcon from '@material-ui/icons/Check';
+import ClearIcon from '@material-ui/icons/Clear';
 import DeleteIcon from '@material-ui/icons/Delete';
-import React, { useEffect } from 'react';
+import EditIcon from '@material-ui/icons/Edit';
+import React, { useEffect, useState } from 'react';
 import SelectService from './addForm/SelectService';
 
 interface Props {
@@ -72,7 +77,21 @@ const EditService: React.FC<Props> = ({
     newServices,
     setNewServices,
 }): React.ReactElement => {
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
+
+    const [name, setName] = useState('');
+    const [fee, setFee] = useState<number | string>('');
+    const [mode, setMode] = useState('');
+    const [duration, setDuration] = useState('');
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingIdx, setEditingIdx] = useState(-1);
+
+    const [openNameSelect, setOpenNameSelect] = useState(false);
+    const [openModeSelect, setOpenModeSelect] = useState(false);
+    const [openDurationSelect, setOpenDurationSelect] = useState(false);
+
+    const serviceDuration = ['30M', '1H', '2H', '3H', '4H'];
 
     useEffect(() => {
         setNewServices(services);
@@ -92,6 +111,44 @@ const EditService: React.FC<Props> = ({
             prevService.splice(idx, 1);
             return [...prevService];
         });
+    };
+
+    const handleCancelEdit = () => {
+        setIsEditing(false);
+        setEditingIdx(-1);
+    };
+
+    const editButtonHandler = (
+        editedData: {
+            name: string;
+            fee: string | number;
+            mode: string;
+            duration: string;
+        },
+        idx: number
+    ) => {
+        setIsEditing(true);
+        setEditingIdx(idx);
+
+        setName(editedData.name);
+        setFee(editedData.fee);
+        setMode(editedData.mode);
+        setDuration(editedData.duration);
+    };
+
+    const handleEdit = (idx: number) => {
+        setNewServices((prev) => {
+            const modifiedArr = [...prev];
+
+            modifiedArr[idx].name = name;
+            modifiedArr[idx].fee = fee;
+            modifiedArr[idx].mode = mode;
+            modifiedArr[idx].duration = duration;
+
+            return modifiedArr;
+        });
+
+        handleCancelEdit();
     };
 
     return (
@@ -128,9 +185,10 @@ const EditService: React.FC<Props> = ({
                                     <TableRow>
                                         <StyledTableCell>Name</StyledTableCell>
                                         <StyledTableCell>Fee</StyledTableCell>
-                                        <StyledTableCell align="right">Mode</StyledTableCell>
-                                        <StyledTableCell align="right">Duration</StyledTableCell>
-                                        <StyledTableCell align="right">Remove</StyledTableCell>
+                                        <StyledTableCell>Mode</StyledTableCell>
+                                        <StyledTableCell>Duration</StyledTableCell>
+                                        <StyledTableCell align="right">Edit</StyledTableCell>
+                                        <StyledTableCell align="right">Delete</StyledTableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
@@ -138,15 +196,179 @@ const EditService: React.FC<Props> = ({
                                         // eslint-disable-next-line no-underscore-dangle
                                         <StyledTableRow key={service._id}>
                                             <StyledTableCell component="th" scope="row">
-                                                {service.name}
+                                                {isEditing && editingIdx === index ? (
+                                                    <FormControl fullWidth>
+                                                        <InputLabel id="demo-controlled-open-select-label">
+                                                            Name
+                                                        </InputLabel>
+                                                        <Select
+                                                            labelId="demo-controlled-open-select-label"
+                                                            id="demo-controlled-open-select"
+                                                            open={openNameSelect}
+                                                            onClose={() => setOpenNameSelect(false)}
+                                                            onOpen={() => setOpenNameSelect(true)}
+                                                            value={name}
+                                                            onChange={(e) => {
+                                                                if (
+                                                                    typeof e.target.value ===
+                                                                    'string'
+                                                                ) {
+                                                                    setName(e.target.value);
+                                                                }
+                                                            }}
+                                                        >
+                                                            {serviceOptions.map((i, idx) => (
+                                                                // eslint-disable-next-line react/no-array-index-key
+                                                                <MenuItem key={idx} value={i}>
+                                                                    {i}
+                                                                </MenuItem>
+                                                            ))}
+                                                        </Select>
+                                                    </FormControl>
+                                                ) : (
+                                                    service.name
+                                                )}
                                             </StyledTableCell>
-                                            <StyledTableCell>{service.fee}</StyledTableCell>
+
+                                            <StyledTableCell>
+                                                {isEditing && editingIdx === index ? (
+                                                    <TextField
+                                                        autoFocus
+                                                        type="number"
+                                                        InputProps={{ inputProps: { min: 0 } }}
+                                                        margin="dense"
+                                                        name="fee"
+                                                        label="Fee"
+                                                        value={fee}
+                                                        onChange={(e) => {
+                                                            if (
+                                                                typeof e.target.value === 'string'
+                                                            ) {
+                                                                setFee(e.target.value);
+                                                            }
+                                                        }}
+                                                    />
+                                                ) : (
+                                                    service.fee
+                                                )}
+                                            </StyledTableCell>
+
+                                            <StyledTableCell>
+                                                {isEditing && editingIdx === index ? (
+                                                    <FormControl fullWidth>
+                                                        <InputLabel id="demo-controlled-open-select-label">
+                                                            Mode
+                                                        </InputLabel>
+                                                        <Select
+                                                            labelId="demo-controlled-open-select-label"
+                                                            id="demo-controlled-open-select"
+                                                            open={openModeSelect}
+                                                            onClose={() => setOpenModeSelect(false)}
+                                                            onOpen={() => setOpenModeSelect(true)}
+                                                            value={mode}
+                                                            onChange={(e) => {
+                                                                if (
+                                                                    typeof e.target.value ===
+                                                                    'string'
+                                                                ) {
+                                                                    setMode(e.target.value);
+                                                                }
+                                                            }}
+                                                        >
+                                                            {['online', 'offline'].map((i) => (
+                                                                <MenuItem key={i} value={i}>
+                                                                    {i}
+                                                                </MenuItem>
+                                                            ))}
+                                                        </Select>
+                                                    </FormControl>
+                                                ) : (
+                                                    service.mode
+                                                )}
+                                            </StyledTableCell>
+
+                                            <StyledTableCell>
+                                                {isEditing && editingIdx === index ? (
+                                                    <FormControl fullWidth>
+                                                        <InputLabel id="demo-controlled-open-select-label">
+                                                            Duration
+                                                        </InputLabel>
+                                                        <Select
+                                                            labelId="demo-controlled-open-select-label"
+                                                            id="demo-controlled-open-select"
+                                                            open={openDurationSelect}
+                                                            onClose={() =>
+                                                                setOpenDurationSelect(false)
+                                                            }
+                                                            onOpen={() =>
+                                                                setOpenDurationSelect(true)
+                                                            }
+                                                            value={duration}
+                                                            onChange={(e) => {
+                                                                if (
+                                                                    typeof e.target.value ===
+                                                                    'string'
+                                                                ) {
+                                                                    setDuration(e.target.value);
+                                                                }
+                                                            }}
+                                                        >
+                                                            {serviceDuration.map((i) => (
+                                                                <MenuItem key={i} value={i}>
+                                                                    {i}
+                                                                </MenuItem>
+                                                            ))}
+                                                        </Select>
+                                                    </FormControl>
+                                                ) : (
+                                                    service.duration
+                                                )}
+                                            </StyledTableCell>
+
                                             <StyledTableCell align="right">
-                                                {service.mode}
+                                                {isEditing && editingIdx === index ? (
+                                                    <div
+                                                        style={{
+                                                            display: 'flex',
+                                                            flexDirection: 'column',
+                                                            alignItems: 'flex-end',
+                                                        }}
+                                                    >
+                                                        <CheckIcon
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                color: 'green',
+                                                                marginBottom: 5,
+                                                            }}
+                                                            onClick={() => handleEdit(index)}
+                                                        />
+
+                                                        <ClearIcon
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                color: 'red',
+                                                            }}
+                                                            onClick={() => handleCancelEdit()}
+                                                        />
+                                                    </div>
+                                                ) : (
+                                                    <EditIcon
+                                                        style={{ cursor: 'pointer' }}
+                                                        onClick={() =>
+                                                            editButtonHandler(
+                                                                {
+                                                                    name: service.name,
+                                                                    fee: service.fee,
+                                                                    mode: service.mode,
+                                                                    duration: service.duration,
+                                                                },
+                                                                index
+                                                            )
+                                                        }
+                                                    />
+                                                )}
                                             </StyledTableCell>
-                                            <StyledTableCell align="right">
-                                                {service.duration}
-                                            </StyledTableCell>
+
                                             <StyledTableCell align="right">
                                                 <DeleteIcon
                                                     color="secondary"
