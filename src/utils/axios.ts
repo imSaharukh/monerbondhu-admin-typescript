c53652900aaa5f4fd0a2fc968eba4dbb75053059
Mohiuddin Khan <mohiuddinkhanromi@gmail.com>
2022-06-16 23:55:08 +0600

    server url changed

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 
 // export const axios = Axios.create({baseURL: 'http://3.22.167.8'});
-// const axios = Axios.create({ baseURL: 'https://blooming-beach-78806.herokuapp.com' });
-const axios = Axios.create({ baseURL: 'http://localhost:8080' });
+const axios = Axios.create({ baseURL: 'https://blooming-beach-78806.herokuapp.com' });
+// const axios = Axios.create({ baseURL: 'http://localhost:8080' });
 // const axios = Axios.create({ baseURL: 'https://18.191.211.173.nip.io/' });
 
 export default axios;
