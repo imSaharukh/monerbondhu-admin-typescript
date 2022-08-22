02434321999542097e6f2327d10022f0f85e272b
Mohiuddin Khan <mohiuddinkhanromi@gmail.com>
2022-08-22 19:33:00 +0600

    prod build

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,8 +1,9 @@
 import Axios from 'axios';
 
 // export const axios = Axios.create({baseURL: 'http://3.22.167.8'});
-const axios = Axios.create({ baseURL: 'https://blooming-beach-78806.herokuapp.com' });
+// const axios = Axios.create({ baseURL: 'https://blooming-beach-78806.herokuapp.com' });
 // const axios = Axios.create({ baseURL: 'http://localhost:8080' });
 // const axios = Axios.create({ baseURL: 'https://18.191.211.173.nip.io/' });
+const axios = Axios.create({ baseURL: 'https://api.monerbondhu.org' });
 
 export default axios;
